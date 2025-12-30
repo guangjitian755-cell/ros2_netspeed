32cbf6e43478131b3ccfcc20a4f56493e09cd9e3
Hikaru Yoshida <guangjitian755@gmail.com>
2025-12-31 02:30:10 +0900

    エントリポイント記述

--- setup.py
+++ setup.py
@@ -24,6 +24,9 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'measur1 = ros2_netspeed.measur1:main',
+            'measur2 = ros2_netspeed.measur2:main',
+            'output = ros2_netspeed.output:main',
         ],
     },
 )
